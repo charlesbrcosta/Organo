129944be3d99c962120e06cd57851293004bbf72
Charles Bruno <charlesbrcosta@gmail.com>
2024-06-15 16:14:59 -0300

    adicionado componente TextField

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Banner } from "./components/Banner/";
+import { TextField } from "./components/TextField";
 
 export function App() {
   return (
     <div className="App">
       <Banner />
+      <TextField label="Nome" placeholder="Digite seu nome" />
+      <TextField label="Cargo" placeholder="Digite seu cargo" />
+      <TextField label="Image" placeholder="Informe o endereço da imagem" />
     </div>
   );
 }
---
+++ src/components/TextField/index.js
@@ -0,0 +1,13 @@
+import styles from './TextField.module.css';
+
+export const TextField = (props) => {
+    
+    const placeholderChange = `${ props.placeholder } ...`;
+
+    return (
+        <div className={ styles.text_field }>
+            <label>{ props.label }</label>
+            <input placeholder={ placeholderChange }/>
+        </div>
+    );
+}
